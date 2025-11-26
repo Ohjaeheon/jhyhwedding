f4b540e17225b394122fac526f9e25d62af15f04
Ohjaeheon <44011493+Ohjaeheon@users.noreply.github.com>
2025-11-26 13:18:42 +0900

    카카오톡api업데이트

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const NAVER_MAP_CLIENT_ID = "calnln6knz"
-export const KAKAO_SDK_JS_KEY = "24a9aefe33d1a2e844cc1504b64fe30b"
+export const KAKAO_SDK_JS_KEY = "022ac872b3a4a9649d82590ff1f9befc"
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL
 export const STATIC_ONLY = import.meta.env.VITE_STATIC_ONLY === "true"
