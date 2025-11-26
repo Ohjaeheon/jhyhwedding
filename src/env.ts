8f67f20f593f521167b7e92aa3570e19ff8b3da3
Ohjaeheon <44011493+Ohjaeheon@users.noreply.github.com>
2025-11-26 10:43:12 +0900

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-export const NAVER_MAP_CLIENT_ID = import.meta.env.VITE_NAVER_MAP_CLIENT_ID
-export const KAKAO_SDK_JS_KEY = import.meta.env.VITE_KAKAO_SDK_JS_KEY
+export const NAVER_MAP_CLIENT_ID = "calnln6knz"
+export const KAKAO_SDK_JS_KEY = "27c28c7c7aadd655ff3b3ef985d7c7b0"
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL
 export const STATIC_ONLY = import.meta.env.VITE_STATIC_ONLY === "true"
