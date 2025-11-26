d9a269de3526bbc468f7629c4012c92c1cd9f29e
Ohjaeheon <44011493+Ohjaeheon@users.noreply.github.com>
2025-11-26 10:48:40 +0900

    키값변경

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const NAVER_MAP_CLIENT_ID = "calnln6knz"
-export const KAKAO_SDK_JS_KEY = "27c28c7c7aadd655ff3b3ef985d7c7b0"
+export const KAKAO_SDK_JS_KEY = "24a9aefe33d1a2e844cc1504b64fe30b"
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL
 export const STATIC_ONLY = import.meta.env.VITE_STATIC_ONLY === "true"
